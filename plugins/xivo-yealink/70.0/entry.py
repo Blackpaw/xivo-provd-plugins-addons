7eed920ea708cbff1593d59d878be3f323f49649
Sylvain Boily <sboily@proformatique.com>
2012-11-21 10:17:05 -0500

    Added support for VP530

--- plugins/xivo-yealink/70.0/entry.py
+++ plugins/xivo-yealink/70.0/entry.py
@@ -3,8 +3,9 @@
 """Plugin for Yealink phones using the 70.0.XX firmware.
 
 The following Yealink phones are supported:
-- T32P
-- T38P
+- T32G
+- T38G
+- VP530
 
 """
 
@@ -12,9 +13,11 @@ common_globals = {}
 execfile_('common.py', common_globals)
 
 
-MODEL_VERSIONS = {u'T32P': u'32.70.0.100',
-                  u'T38P': u'38.70.0.100'}
-COMMON_FILES = [('y000000000032.cfg', u'32.70.0.100.rom'),
+MODEL_VERSIONS = {u'T32G': u'32.70.0.100',
+                  u'T38G': u'38.70.0.100',
+                  u'VP530' : u'23.70.0.40'}
+COMMON_FILES = [('y000000000023.cfg', u'23.70.0.40-Yealink.rom'),
+                ('y000000000032.cfg', u'32.70.0.100.rom'),
                 ('y000000000038.cfg', u'38.70.0.100.rom')]
 
 
