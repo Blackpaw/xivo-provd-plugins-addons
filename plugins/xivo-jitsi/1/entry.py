fce9d24a4387dc923b0a1a811335620f8c7f89ee
Gregory Eric Sanderson <gsanderson@avencall.com>
2013-01-22 16:29:59 -0500

    adjust license in python file headers

--- plugins/xivo-jitsi/1/entry.py
+++ plugins/xivo-jitsi/1/entry.py
@@ -1,25 +1,24 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011-2013 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 """Plugin for the Jitsi softphone in version 1.x.
 
 """
 
-__license__ = """
-    Copyright (C) 2011  Avencall
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
 
 # provisioning url to use in Jitsi: http://<provd_ip:provd_port>/jitsi?uuid=${uuid}
 
