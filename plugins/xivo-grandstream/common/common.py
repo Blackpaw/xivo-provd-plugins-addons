fd14f3b1f621da7455d26aa9ab85b909f0ad7edd
Jean Aunis <jaunis@avencall.com>
2013-09-06 14:04:57 +0200

    improved support for grandstream gxp2100

--- plugins/xivo-grandstream/common/common.py
+++ plugins/xivo-grandstream/common/common.py
@@ -33,6 +33,21 @@ from twisted.internet import defer, threads
 
 logger = logging.getLogger('plugin.xivo-grandstream')
 
+TZ_NAME = { 'Europe/Paris': 'CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00' }
+LOCALE = {
+        u'de_DE': 'de',
+        u'es_ES': 'es',
+        u'fr_FR': 'fr',
+        u'fr_CA': 'fr',
+        u'it_IT': 'it',
+        u'nl_NL': 'nl'
+    }
+
+FUNCKEY_TYPES = {
+        u'speeddial': 0,
+        u'blf': 1,
+        u'park': 9
+    }
 
 class BaseGrandstreamHTTPDeviceInfoExtractor(object):
 
@@ -121,6 +136,9 @@ class BaseGrandstreamPlugin(StandardPlugin):
     def configure(self, device, raw_config):
         self._check_config(raw_config)
         self._check_device(device)
+        self._add_timezone(raw_config)
+        self._add_locale(raw_config)
+        self._add_fkeys(raw_config)
         filename = self._dev_specific_filename(device)
         tpl = self._tpl_helper.get_dev_template(filename, device)
 
@@ -148,3 +166,43 @@ class BaseGrandstreamPlugin(StandardPlugin):
                 return defer.fail(Exception('Incompatible sync service: %s' % sync_service))
             else:
                 return threads.deferToThread(sync_service.sip_notify, ip, 'check-sync')
+
+    def _add_timezone(self, raw_config):
+        if u'timezone' in raw_config and raw_config[u'timezone'] in TZ_NAME:
+            raw_config[u'XX_timezone'] = TZ_NAME[raw_config[u'timezone']]
+        else:
+            raw_config['timezone'] = TZ_NAME['Europe/Paris']
+
+    def _add_locale(self, raw_config):
+       locale = raw_config.get(u'locale')
+       if locale in LOCALE:
+            raw_config[u'XX_locale'] = LOCALE[locale]
+
+    def _add_fkeys(self, raw_config):
+        lines = []
+        for funckey_no, funckey_dict in raw_config[u'funckeys'].iteritems():
+            i_funckey_no = int(funckey_no)
+            funckey_type = funckey_dict[u'type']
+            if funckey_type not in FUNCKEY_TYPES:
+                logger.info('Unsupported funckey type: %s', funckey_type)
+                continue
+            type_code = u'P32%s' % (i_funckey_no + 2)
+            lines.append(self._format_line(type_code, FUNCKEY_TYPES[funckey_type]))
+            line_code = self._format_code(3*i_funckey_no - 2)
+            lines.append(self._format_line(line_code, int(funckey_dict[u'line']) - 1))
+            if u'label' in funckey_dict : 
+                label_code = self._format_code(3*i_funckey_no - 1)
+                lines.append(self._format_line(label_code, funckey_dict[u'label']))
+            value_code = self._format_code(3*i_funckey_no)
+            lines.append(self._format_line(value_code, funckey_dict[u'value']))
+        raw_config[u'XX_fkeys'] = u'\n'.join(lines)
+
+    def _format_line(self, code, value):
+        return u'    <%s>%s</%s>' % (code, value, code)
+
+    def _format_code(self, code):
+        if code >= 10:
+            str_code = str(code)
+        else:
+            str_code = u'0%s' % code
+        return u'P3%s' % str_code
