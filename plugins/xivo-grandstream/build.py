d66751a3566f43c9cff28d8cbbeb4beaa04325c4
Etienne Lessard <elessard@avencall.com>
2014-06-27 13:49:25 -0400

    remove old xivo-grandstream-1.0.1.27 plugin

--- plugins/xivo-grandstream/build.py
+++ plugins/xivo-grandstream/build.py
@@ -21,15 +21,6 @@
 from subprocess import check_call
 
 
-@target('1.0.5.26', 'xivo-grandstream-1.0.5.26')
-def build_1_0_5_12(path):
-    check_call(['rsync', '-rlp', '--exclude', '.*',
-                '--include', '/templates/*',
-                'common/', path])
-
-    check_call(['rsync', '-rlp', '--exclude', '.*',
-                '1.0.5.26/', path])
-
 @target('1.0.1.40', 'xivo-grandstream-1.0.1.40')
 def build_1_0_1_40(path):
     check_call(['rsync', '-rlp', '--exclude', '.*',
@@ -38,6 +29,7 @@ def build_1_0_1_40(path):
 
     check_call(['rsync', '-rlp', '--exclude', '.*',
                 '1.0.1.40/', path])
+
 
 @target('1.0.1.27', 'xivo-grandstream-1.0.1.27')
 def build_1_0_1_27(path):
